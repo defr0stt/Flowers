0186102a7499fef8cb2d8f3fe6a87a549259f1df
defr0stt <dima.sb713@gmail.com>
2021-11-30 17:44:55 +0200

    Start of creating "Flowers menu"

--- src/main/java/JavaFXpart/ActionFlowers/ActionFlowers.java
+++ src/main/java/JavaFXpart/ActionFlowers/ActionFlowers.java
@@ -1,12 +1,32 @@
 package JavaFXpart.ActionFlowers;
 
 import JavaFXpart.Receiver.Command;
+import JavaFXpart.StartMenu;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+import java.io.FileNotFoundException;
 
 public class ActionFlowers implements Command {
 
     @Override
     public void execute()
     {
-        
+        Text welcomeFlowersText = new StartMenu().textConstructor("Flowers  menu",1,2,50.0,385.0);
+        welcomeFlowersText.setStyle("-fx-font-size: 24px;");
+
+        ImageView backImageView = null;
+        try {
+
+
+            StartMenu.pane = new StartMenu().authorPlusImagePulsing(welcomeFlowersText);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        StartMenu.pane = new StartMenu().sumAllElements((AnchorPane) StartMenu.pane, new Node[]{});
+        new StartMenu().sceneStarter(StartMenu.pane, StartMenu.defaultStage);
     }
 }
