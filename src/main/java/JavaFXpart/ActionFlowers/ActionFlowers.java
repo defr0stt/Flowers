c7f20d07d8d66c2761c28c286c5b6d8087d2a904
defr0stt <dima.sb713@gmail.com>
2021-12-01 00:41:18 +0200

    Added photos in "Flower menu"

--- src/main/java/JavaFXpart/ActionFlowers/ActionFlowers.java
+++ src/main/java/JavaFXpart/ActionFlowers/ActionFlowers.java
@@ -14,19 +14,33 @@ public class ActionFlowers implements Command {
     @Override
     public void execute()
     {
-        Text welcomeFlowersText = new StartMenu().textConstructor("Flowers  menu",1,2,50.0,385.0);
+        Text welcomeFlowersText = new StartMenu().textConstructor("Flower  menu",1,2,50.0,385.0);
         welcomeFlowersText.setStyle("-fx-font-size: 24px;");
 
-        ImageView backImageView = null;
+        ImageView backImageView1 = null;
+        ImageView backImageView2 = null;
+        ImageView backImageView3 = null;
+        ImageView backImageView4 = null;
         try {
-
+            backImageView1 = new StartMenu().imageConstructor("resources/actionFlower1.png",1,2,230.0,120.0);
+            backImageView2 = new StartMenu().imageConstructor("resources/actionFlower2.png",1,2,230.0,680.0);
+            backImageView2.setFitHeight(128);
+            backImageView2.setFitWidth(128);
+            backImageView3 = new StartMenu().imageConstructor("resources/actionFlower3.png",1,2,55.0,555.0);
+            backImageView3.setFitHeight(32);
+            backImageView3.setFitWidth(12);
+            backImageView4 = new StartMenu().imageConstructor("resources/actionFlower3.png",1,2,55.0,340.0);
+            backImageView4.setFitHeight(32);
+            backImageView4.setFitWidth(12);
+            backImageView4.setScaleX(-1);
 
             StartMenu.pane = new StartMenu().authorPlusImagePulsing(welcomeFlowersText);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
-        StartMenu.pane = new StartMenu().sumAllElements((AnchorPane) StartMenu.pane, new Node[]{});
+        StartMenu.pane = new StartMenu().sumAllElements((AnchorPane) StartMenu.pane,
+                new Node[]{backImageView1, backImageView2, backImageView3, backImageView4});
         new StartMenu().sceneStarter(StartMenu.pane, StartMenu.defaultStage);
     }
 }
